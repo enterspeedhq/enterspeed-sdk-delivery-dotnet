b971df0a2b501c90ccb87b9fa707b6becfd780d5
ajk <ajk@enterspeed.com>
2022-05-24 09:40:40 +0200

    jespers PR comment

--- src/Enterspeed.Delivery.Sdk/Domain/Helpers/QueryStringHelper.cs
+++ src/Enterspeed.Delivery.Sdk/Domain/Helpers/QueryStringHelper.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Web;
+
 namespace Enterspeed.Delivery.Sdk.Domain.Helpers
 {
-    internal class QueryStringHelper
+     internal class QueryStringHelper
     {
-        private readonly IList<string> _keys = new List<string>();
-        private readonly IList<string> _values = new List<string>();
+        private readonly List<KeyValuePair<string, string>> _parameters = new List<KeyValuePair<string, string>>();
 
         public override string ToString()
         {
-            var builder = new StringBuilder();
-            for (var i = 0; i < _keys.Count; i++)
-            {
-                builder.Append(i == 0 ? $"?{_keys[i]}={_values[i]}" : $"&{_keys[i]}={_values[i]}");
-            }
-
-            return builder.ToString();
+            var keyValues = _parameters.Select(
+                kvp => $"{HttpUtility.UrlEncode(kvp.Key)}={HttpUtility.UrlEncode(kvp.Value)}");
+            return string.Concat("?", string.Join("&", keyValues));
         }
 
         public void Add(string key, string value)
@@ -31,8 +28,7 @@ namespace Enterspeed.Delivery.Sdk.Domain.Helpers
                 throw new ArgumentNullException(nameof(value), "Value cannot be an empty string or null");
             }
 
-            _keys.Add(key);
-            _values.Add(value);
+            _parameters.Add(new KeyValuePair<string, string>(key, value));
         }
     }
 }
